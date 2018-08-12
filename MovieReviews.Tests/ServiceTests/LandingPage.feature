7c9963b9f6882d42b46ef4c2d9b0d3d9bcb46f77
rohit <rohit@ROHITP>
2018-08-12 10:34:20 +0530

    Latest

---
+++ MovieReviews.Tests/ServiceTests/LandingPage.feature
@@ -0,0 +1,10 @@
+﻿Feature: LandingPage
+	In order to view reviews of latest movies
+	as a user
+	I want a list of latest movie reviews
+
+@mytag
+Scenario: Get All Movies
+	Given I have reached login page	
+	When I check all the movies
+	Then I should view all the movies
---
+++ MovieReviews.Tests/ServiceTests/LandingPageSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace MovieReviews.Tests
+{
+    [Binding]
+    public class LandingPageSteps
+    {
+        [Given(@"I have reached login page")]
+        public void GivenIHaveReachedLoginPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I check all the movies")]
+        public void WhenICheckAllTheMovies()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should view all the movies")]
+        public void ThenIShouldViewAllTheMovies()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
